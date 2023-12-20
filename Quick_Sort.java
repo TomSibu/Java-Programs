b754603ac0e5f575528c1e85e4c73e8193335626
Tom Sibu <118618366+TomSibu@users.noreply.github.com>
2023-12-21 01:40:54 +0530

    Update Quick_Sort.java

--- Quick_Sort.java
+++ Quick_Sort.java
@@ -9,62 +9,54 @@
  * Roll No			:	63
 ***********************************************************************************************/
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Quick_Sort {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    	public static void main(String[] args) {
+        	Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the number of names: ");
-        int n = scanner.nextInt();
-        scanner.nextLine(); 
+        	System.out.print("Enter the number of names: ");
+        	int n = scanner.nextInt();
+        	scanner.nextLine();
 
-        String[] names = new String[n];
+        	ArrayList<String> names = new ArrayList<>();
+        	System.out.println("Enter the names:");
+        	for (int i = 0; i < n; i++) {
+           	System.out.print("Name " + (i + 1) + ": ");
+            	names.add(scanner.nextLine());
+        	}
 
-        System.out.println("Enter the names:");
+        	quickSort(names, 0, n - 1);
+        	System.out.println("\nSorted Names (in ascending order):");
+        	for (String name : names) {
+            	System.out.println(name);
+        		}
+    	}
 
-        for (int i = 0; i < n; i++) {
-            System.out.print("Name " + (i + 1) + ": ");
-            names[i] = scanner.nextLine();
-        }
+    	private static void quickSort(ArrayList<String> arr, int low, int high) {
+        		if (low < high) {
+            		int partitionIndex = partition(arr, low, high);
+            		quickSort(arr, low, partitionIndex - 1);
+            		quickSort(arr, partitionIndex + 1, high);
+        		}
+    	}
 
-        quickSort(names, 0, n - 1);
+    	private static int partition(ArrayList<String> arr, int low, int high) {
+        		String pivot = arr.get(high);
+        		int i = low - 1;
 
-        System.out.println("\nSorted Names (in ascending order):");
-        for (String name : names) {
-            System.out.println(name);
-        }
-
-        scanner.close();
-    }
-
-    private static void quickSort(String[] arr, int low, int high) {
-        if (low < high) {
-            int partitionIndex = partition(arr, low, high);
-
-            quickSort(arr, low, partitionIndex - 1);
-            quickSort(arr, partitionIndex + 1, high);
-        }
-    }
-
-    private static int partition(String[] arr, int low, int high) {
-        String pivot = arr[high];
-        int i = low - 1;
-
-        for (int j = low; j < high; j++) {
-            if (arr[j].compareTo(pivot) < 0) {
-                i++;
-
-                String temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-
-        String temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
-
-        return i + 1;
-    }
+        		for (int j = low; j < high; j++) {
+            		if (arr.get(j).compareTo(pivot) < 0) {
+                			i++;
+                			String temp = arr.get(i);
+                			arr.set(i, arr.get(j));
+                			arr.set(j, temp);
+            		}
+        		}
+        		String temp = arr.get(i + 1);
+        		arr.set(i + 1, arr.get(high));
+        		arr.set(high, temp);
+        		return i + 1;
+    	}
 }
